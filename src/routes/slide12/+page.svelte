ef06c584e79aeb3acb434f99209237755b06975e
pomesh <pomesh@student.chalmers.se>
2025-05-23 17:14:05 +0200

    added qr code to end page

--- src/routes/slide12/+page.svelte
+++ src/routes/slide12/+page.svelte
@@ -2,6 +2,9 @@
     import StaggeredText from "../../components/StaggeredText.svelte";
     import Title from "../../components/Title.svelte";
 
+    import qrLogo from "$lib/assets/qrcode_korpi.netlify.app.png";
+    import { fade } from "svelte/transition";
+
     let text = [
         ["Peter Ljunglöf", "For helping us with the backend and understanding linguistics."],
         ["Språkbanken", "For taking their time to meet with us and explain Korp API and how Korp works."],
@@ -10,6 +13,35 @@
 </script>
 
 <style>
+    .maincontent {
+        display: flex;
+    }
+
+    .qr-code-column {
+        display: inline-block;
+    }
+
+    .desc-text {
+        font-size: 2rem;
+        font-weight: bold;
+        color: darkcyan;
+        text-align: center;
+    }
+
+    .korpi-link {
+        font-size: 1.2rem;
+        font-style: italic;
+    }
+    
+    img {
+        margin: 0;
+    }
+
+    .qr-code-column {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
 </style>
 
@@ -18,6 +50,13 @@
     subtitle="Thank you to everyone who worked on the project!" 
     nextSlide="/slide13"
     prevSlide="/slide11"/>
-<div>
-    <StaggeredText --fontsize="1.4rem" input_text={text} />
+<div class="maincontent">
+    <div class="text-column">
+        <StaggeredText --fontsize="1.4rem" input_text={text} />
+    </div>
+    <div in:fade={{duration: 1000}} class="qr-code-column">
+        <p class="desc-text">Test Korpi Right Now!</p>
+        <img src={qrLogo} alt="qrcode"/>
+        <a class="korpi-link" href="https://korpi.netlify.app/">korpi.netlify.app</a>
+    </div>
 </div>

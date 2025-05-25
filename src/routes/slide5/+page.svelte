d9429024244266ff64e79df4ff0ab6070b084de0
pomesh <pomesh@student.chalmers.se>
2025-05-25 13:29:10 +0200

    added slide for focus groups

--- src/routes/slide5/+page.svelte
+++ src/routes/slide5/+page.svelte
@@ -1,6 +1,5 @@
 <script>
     import Graph from "../../components/Graph.svelte";
-    import StaggeredText from "../../components/StaggeredText.svelte";
     import Title from "../../components/Title.svelte";
 
     import { fade } from "svelte/transition";
@@ -11,18 +10,18 @@
     let youngerData = $state([0, 0, 0, 0, 0]);
 
     let categories = [
-        ["Useful", "How useful do you believe Korpi to be?", [0, 0, 3, 0, 1], [1, 0, 1, 0, 1]],
-        ["Responsiveness", "How useful do you believe Korpi to be?", [0, 0, 1, 2, 1], [0, 0, 1, 1, 0]],
-        ["Speed(Searches)", "How useful do you believe Korpi to be?", [0, 0, 2, 0, 2], [0, 1, 0, 1, 1]],
-        ["Navigation", "How useful do you believe Korpi to be?", [0, 0, 0, 3, 1], [0, 1, 0, 1, 1]],
-        ["Design", "How useful do you believe Korpi to be?", [0, 0, 1, 1, 2], [0, 0, 1, 1, 1]],
-        ["Interesting", "How useful do you believe Korpi to be?", [0, 0, 1, 1, 2], [1, 0, 0, 2, 0]],
-        ["Ease of understanding", "How useful do you believe Korpi to be?", [0, 0, 4, 0, 0], [0, 1, 2, 0, 0]],
-        ["Experience when using Korpi", "How useful do you believe Korpi to be?", [0, 0, 1, 3, 0], [0, 0, 1, 2, 0]],
-        ["Presentation of Information", "How useful do you believe Korpi to be?", [0, 0, 0, 3, 1], [0, 0, 2, 1, 0]],
-        ["Presentation of Statistics", "How useful do you believe Korpi to be?", [0, 0, 1, 2, 1], [0, 0, 1, 1, 1]],
-        ["Presentation of Results", "How useful do you believe Korpi to be?", [0, 0, 0, 2, 2], [0, 1, 0, 2, 0]],
-        ["Overall Impression", "How useful do you believe Korpi to be?", [0, 0, 0, 3, 1], [0, 1, 0, 1, 0]],
+        ["Useful", "", [0, 0, 3, 0, 1], [1, 0, 1, 0, 1]],
+        ["Responsiveness", "", [0, 0, 1, 2, 1], [0, 0, 1, 1, 0]],
+        ["Speed(Searches)", "", [0, 0, 2, 0, 2], [0, 1, 0, 1, 1]],
+        ["Navigation", "", [0, 0, 0, 3, 1], [0, 1, 0, 1, 1]],
+        ["Design", "", [0, 0, 1, 1, 2], [0, 0, 1, 1, 1]],
+        ["Interesting", "", [0, 0, 1, 1, 2], [1, 0, 0, 2, 0]],
+        ["Ease of understanding", "", [0, 0, 4, 0, 0], [0, 1, 2, 0, 0]],
+        ["Experience when using Korpi", "", [0, 0, 1, 3, 0], [0, 0, 1, 2, 0]],
+        ["Presentation of Information", "", [0, 0, 0, 3, 1], [0, 0, 2, 1, 0]],
+        ["Presentation of Statistics", "", [0, 0, 1, 2, 1], [0, 0, 1, 1, 1]],
+        ["Presentation of Results", "", [0, 0, 0, 2, 2], [0, 1, 0, 2, 0]],
+        ["Overall Impression", "", [0, 0, 0, 3, 1], [0, 1, 0, 1, 0]],
     ]
 
     function onkeydown (e) {
@@ -47,18 +46,17 @@
     }
 
     .alltext {
-        font-size: var(--fontsize);
+        font-size: 1.8rem;
         margin-top: 20px;
     }
     .maintext {
         font-weight: bold;
-        color: rgb(59, 5, 5);
+        color: var(--text-secondary-color)
     }
 
     .maintext:hover {
         cursor: pointer;
         color: orangered;
-        text-shadow: 1px 9px 14px rgba(0,0,0,0.6);
     }
 
     .subtext {
@@ -68,16 +66,20 @@
     .current-aspect-text {
         text-align: center;
         font-weight: bold;
-        font-size: 1.5rem;
+        font-size: 2.2rem;
         margin: 10px;
         color: darkslategray;
+    }
+
+    .rightside {
+        margin: 100px;
     }
 
 </style>
 
 <svelte:window onkeydown={onkeydown} />
 
-<Title title="Results" nextSlide="/slide6" prevSlide="/slide4"/>
+<Title title="Results" subtitle="What the focus groups thought of Korpi!" nextSlide="/slide6" prevSlide="/slide4"/>
 <div class="slidecontent">
     <div class="aspectstext">
     <!-- svelte-ignore a11y_no_static_element_interactions -->
--- src/routes/slide9/+page.svelte
+++ src/routes/slide9/+page.svelte
@@ -1,8 +1,58 @@
 <script>
+    import { fade } from "svelte/transition";
+    import StaggeredText from "../../components/StaggeredText.svelte";
     import Title from "../../components/Title.svelte";
+
+    let input_text =[["", "There were three separate meetings where the particpants could evaluate Korp and Korpi as well as provide feedback."]];
+
 </script>
 
 <style>
+
+    .slidecontent {
+        margin: 50px;
+        height: 600px;
+    }
+
+    .maincontent {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        height: inherit;
+    }
+
+    .circle {
+        width: 400px;
+        height: 400px;
+        border-radius: 50%;
+        color: var(--text-secondary-color);
+        font-weight: bold;
+        font-size: 2rem;
+        border: 2px solid gray;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        text-align: center;
+
+        transition: box-shadow 0.3s ease;
+    }
+
+    .circle:hover {
+        box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
+        cursor: pointer;
+    }
+
+    .circle-left {
+        background-color: #ffadad;
+    }
+
+    .circle-right {
+        background-color: #dedaf4;
+    }
+
+    .staggertext-container {
+        text-align: center;
+    }
 
 </style>
 
@@ -14,7 +64,19 @@
 ></Title>
 
 <div class="slidecontent">
+    <div class="staggertext-container">
+        <StaggeredText {input_text} --fontsize="2.3rem"></StaggeredText>
+    </div>
     <div class="maincontent">
-        CONTENT
+        <div class="content-column" in:fade={{duration:1000}}>
+            <div class="circle circle-right">
+                4 Participants <br> Age less than 40
+            </div>
+        </div>
+        <div class="content-column">
+            <div class="circle circle-left" in:fade={{duration:1000, delay:100}}>
+                3 Participants <br> Age greater than 60
+            </div>
+        </div>
     </div> 
 </div>

235e602dc12ca6857f40831d7fe4047fa454e639
pomesh <pomesh@student.chalmers.se>
2025-05-25 16:12:35 +0200

    added border to results page

--- src/routes/slide5/+page.svelte
+++ src/routes/slide5/+page.svelte
@@ -73,6 +73,16 @@
 
     .rightside {
         margin: 100px;
+        width: inherit;
+        border: 2px solid gray;
+        border-radius: 15px;
+        padding: 40px;
+
+        transition: box-shadow 0.3s ease;
+    }
+    
+    .rightside:hover {
+         box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
     }
 
 </style>

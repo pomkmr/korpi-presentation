76b4ee1df063fa28df7d93200e7b95e995573576
pomesh <pomesh@student.chalmers.se>
2025-05-25 12:53:08 +0200

    added hardcoded defs to corpus

--- src/routes/slide2/+page.svelte
+++ src/routes/slide2/+page.svelte
@@ -4,26 +4,43 @@
     import StaggeredText from "../../components/StaggeredText.svelte";
     import Title from "../../components/Title.svelte";
 
-    let input_text = [["", "Here is the prologue of Romeo and Juliet by Shakespeare."],
-                      ["", "Every word in a corpus is annotated with certain attributes."],];
+    let input_text = [["", "Every word in a corpus is annotated with certain attributes."],];
 
     let page_text = "Two households, both alike in dignity, In fair Verona, where we lay our scene, From ancient grudge break to new mutiny, Where civil blood makes civil hands unclean. From forth the fatal loins of these two foes. A pair of star-cross'd lovers take their life; Whose misadventured piteous overthrows Do with their death bury their parents' strife. The fearful passage of their death-mark'd love, And the continuance of their parents' rage, Which, but their children's end, nought could remove, Is now the two hours' traffic of our stage; The which if you with patient ears attend, What here shall miss, our toil shall strive to mend.".split(' ');
     
     let selectedWord = $state('');
-    let base_url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-
-    let dictData = $state('');
+    let selectedWordDef = $state('');
+    let selectedWordClass = $state('');
 
     function onWordClick(e) {
         console.log('word clicked', e.target.innerText);
         selectedWord = e.target.innerText;
 
-        console.log('fetching: ', base_url+selectedWord);
-        fetch(base_url+selectedWord)
-            .then((res) => res.json())
-            .then((res) => dictData = res)
+        if (selectedWord === "households,") {
+            selectedWord = "households";
+            selectedWordDef = "those who dwell under the same roof and compose a family"
+            selectedWordClass = "noun";
+        }
 
-        console.log(dictData);
+        if (selectedWord === "ancient") {
+            selectedWord = "ancient";
+            selectedWordDef = "having had an existence of many years"
+            selectedWordClass = "noun";
+        }
+
+        if (selectedWord === "misadventured") {
+            selectedWord = "misadventured";
+            selectedWordDef = "unfortunate; marked or accompanied by or resulting in misfortune"
+            selectedWordClass = "adjective";
+        }
+
+        if (selectedWord === "shall") {
+            selectedWord = "shall";
+            selectedWordDef = "used to express what is inevitable or seems likely to happen in the future"
+            selectedWordClass = "verb";
+        }
+
+
     }
 
 </script>
@@ -68,7 +85,7 @@
         
         width: 800px;
         padding: 20px 100px;
-        height: 200px;
+        height: 250px;
         margin-top: 10px;
 
 
@@ -129,11 +146,8 @@ prevSlide="/slide1" nextSlide="slide3" />
         </div>
         <div in:fade={{duration:1000, delay:1500}} class="card">
             <p class="word-text">Word: <span class="word-text-entry">{selectedWord}</span></p>
-            <p class="word-text">Meaning: <span class="word-text-entry">{selectedWord}</span></p>
-            <p class="word-text">Word Class: <span class="word-text-entry">{selectedWord}</span></p>
-            <div>
-                {dictData[0]}
-            </div>
+            <p class="word-text">Meaning: <span class="word-text-entry">{selectedWordDef}</span></p>
+            <p class="word-text">Word Class: <span class="word-text-entry">{selectedWordClass}</span></p>
         </div>
     </div>
 </div>
